0b212c6f75bbb51f706b72b0a7441a7cee2f435f
Emma Lundmark <h24lunem@du.se>
2025-10-20 17:28:23 +0200

    Implementerade filerna PieceColor, Player och GameStatus

---
+++ Checkers/CheckersGame/Models/GameStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.CheckersGame.Models
+{
+    // Enumdefinition för GameStatus
+    public enum GameStatus
+    {
+        WaitingToStart,
+        InProgress,
+        Completed,
+        Abandoned
+    }
+}
