ced9c4ae6121b0618ee8fa473f8152255660b6d6
AndreasL85 <h24andrl@du.se>
2025-10-20 11:32:43 +0200

    Add project files.

---
+++ Checkers/CheckersGame/Models/Board.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.CheckersGame.Models
+{
+    internal class Board
+    {
+    }
+}
---
+++ Checkers/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Checkers.Checkers();
+game.Run();
