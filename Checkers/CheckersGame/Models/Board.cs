51c50ae5a1f6bf0d6728d1c46b3c39241eb8d318
joppess <h24jolid@du.se>
2025-10-20 12:31:22 +0200

    la till instruktioner osv i board

--- Checkers/CheckersGame/Models/Board.cs
+++ Checkers/CheckersGame/Models/Board.cs
@@ -8,5 +8,29 @@ namespace Checkers.CheckersGame.Models
 {
     internal class Board
     {
+        List<Vector2> cordinates = new List<Vector2>();
+        public Board()
+        {
+
+        }
     }
 }
+        // work in progress
+
+              // get piece som säger vilken piece som är på den positionen 
+                // board ska anropa move piece
+               // new piece
+                // piece ska kunna vara nulla
+                // vad är det för piece på platsen  
+                // PlacePiece inistialiseras när vi gör spelpllan och göra den med for loop
+                // MovePiece - flytta pjäs från en position från en annan
+                // Boarden bryr sig inte om nåt annat
+                
+                
+
+
+// prio varje klass och funktion finns inte vad som finns i dom
+
+
+
+
