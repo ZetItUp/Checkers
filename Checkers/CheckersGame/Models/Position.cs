6f4e07294e8f33c7390cebe8d49250f19b2fff27
Emma Lundmark <h24lunem@du.se>
2025-10-23 19:24:23 +0200

    Position uppdaterad

--- Checkers/CheckersGame/Models/Position.cs
+++ Checkers/CheckersGame/Models/Position.cs
@@ -23,5 +23,29 @@ namespace Checkers.CheckersGame.Models
         {
             return Row >= 0 && Row < size && Column >= 0 && Column < size;
         }
+        public bool Equals(Position other)
+        {
+            return Row == other.Row && Column == other.Column;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Position otherPosition)
+            {
+                return Equals(otherPosition);
+            }
+            return false;
+        }
+
+        // Åsidosätter GetHashCode() för att matcha värdejämförelsen
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Column);
+        }
+
+        public override string ToString()
+        {
+            return $"({Row}, {Column})";
+        }
     }
 }
