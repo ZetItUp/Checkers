c19dc51edee28026ca58b0846a86c5cca196223d
AndreasL85 <h24andrl@du.se>
2025-11-05 10:30:51 +0100

    RuleSetDto

--- Checkers/CheckersGame/Validation/RuleSetDto.cs
+++ Checkers/CheckersGame/Validation/RuleSetDto.cs
@@ -7,8 +7,12 @@ using System.Text.Json.Serialization;
 
 namespace Checkers.CheckersGame.Validation.Config
 {
+    /// <summary>
+    /// Klass för att representera RuleSet-dataöverföringsobjekt (DTO) från JSON
+    /// </summary>
     internal sealed class RuleSetDto
     {
+        // Sätt JsonPropertyName attribut för att matcha JSON-egenskaper
         [JsonPropertyName("boardSize")]
         public int? BoardSize { get; set; }
 
